82073e3d2fa6757b6739533ae668e9ae1f1bd95f
yaling888 <73897884+yaling888@users.noreply.github.com>
2022-11-28 19:01:06 +0800

    Fix: wireguard udp

--- adapter/outbound/wireguard.go
+++ adapter/outbound/wireguard.go
@@ -9,7 +9,6 @@ import (
 	"math/rand"
 	"net"
 	"net/netip"
-	"runtime"
 	"strconv"
 	"strings"
 	"sync"
@@ -26,7 +25,7 @@ import (
 	"github.com/Dreamacro/clash/transport/wireguard"
 )
 
-const dialTimeout = 8 * time.Second
+const dialTimeout = 10 * time.Second
 
 type WireGuard struct {
 	*Base
@@ -36,8 +35,8 @@ type WireGuard struct {
 	bind      *wireguard.WgBind
 
 	dialer     *wgDialer
-	endpoint   netip.AddrPort
-	localIPs   []netip.Addr
+	localIP    netip.Addr
+	localIPv6  netip.Addr
 	dnsServers []netip.Addr
 	uapiConf   []string
 	threadId   string
@@ -66,7 +65,7 @@ type WireGuardOption struct {
 func (w *WireGuard) DialContext(ctx context.Context, metadata *C.Metadata, opts ...dialer.Option) (C.Conn, error) {
 	w.up()
 	if w.upErr != nil {
-		return nil, fmt.Errorf("apply wireguard proxy %s config failure, cause: %w", w.threadId, w.upErr)
+		return nil, fmt.Errorf("apply wireguard proxy %s config error: %w", w.threadId, w.upErr)
 	}
 	w.dialer.options = opts
 
@@ -84,33 +83,34 @@ func (w *WireGuard) DialContext(ctx context.Context, metadata *C.Metadata, opts 
 	if c == nil {
 		return nil, errors.New("conn is nil")
 	}
-	return NewConn(c, w), nil
+	return NewConn(&wgConn{c}, w), nil
 }
 
-func (w *WireGuard) ListenPacketContext(_ context.Context, _ *C.Metadata, opts ...dialer.Option) (C.PacketConn, error) {
+func (w *WireGuard) ListenPacketContext(_ context.Context, metadata *C.Metadata, opts ...dialer.Option) (C.PacketConn, error) {
 	w.up()
 	if w.upErr != nil {
 		return nil, fmt.Errorf("apply wireguard proxy %s config failure, cause: %w", w.threadId, w.upErr)
 	}
 	w.dialer.options = opts
 
-	pc, err := w.netStack.ListenUDPAddrPort(w.endpoint)
+	var lAddr netip.Addr
+	if metadata.DstIP.Is6() {
+		lAddr = w.localIPv6
+	} else {
+		lAddr = w.localIP
+	}
+
+	pc, err := w.netStack.ListenUDPAddrPort(netip.AddrPortFrom(lAddr, 0))
 	if err != nil {
 		return nil, err
 	}
 	if pc == nil {
 		return nil, errors.New("packetConn is nil")
 	}
-	return NewPacketConn(pc, w), nil
+	return NewPacketConn(&wgPConn{pc}, w), nil
 }
 
-func (w *WireGuard) up() {
-	w.upOnce.Do(func() {
-		w.upErr = initWireGuard(w)
-	})
-}
-
-func (w *WireGuard) down() {
+func (w *WireGuard) Cleanup() {
 	w.downOnce.Do(func() {
 		if w.wgDevice != nil {
 			w.wgDevice.Close()
@@ -118,16 +118,71 @@ func (w *WireGuard) down() {
 	})
 }
 
-type wgDialer struct {
-	options []dialer.Option
+func (w *WireGuard) up() {
+	w.upOnce.Do(func() {
+		w.upErr = w.init()
+	})
 }
 
-func (d *wgDialer) DialContext(ctx context.Context, network string, address netip.AddrPort) (net.Conn, error) {
-	return dialer.DialContext(ctx, network, address.String(), d.options...)
-}
+func (w *WireGuard) init() error {
+	host, port, _ := net.SplitHostPort(w.Base.Addr())
+	tryTimes := 0
 
-func (d *wgDialer) ListenPacket(ctx context.Context, _ netip.AddrPort) (net.PacketConn, error) {
-	return dialer.ListenPacket(ctx, "udp", "", d.options...)
+lookup:
+	endpointIP, err := resolver.ResolveProxyServerHost(host)
+	if err != nil {
+		if tryTimes < 5 {
+			tryTimes++
+			time.Sleep(2 * time.Second)
+			goto lookup
+		}
+		return fmt.Errorf("parse server endpoint [%s] failure, cause: %w", w.Base.Addr(), err)
+	}
+
+	p, _ := strconv.Atoi(port)
+	endpoint := netip.AddrPortFrom(endpointIP, uint16(p))
+	w.uapiConf = append(w.uapiConf, fmt.Sprintf("endpoint=%s", endpoint))
+
+	wgBind := wireguard.NewWgBind(context.Background(), w.dialer, endpoint)
+
+	localIPs := make([]netip.Addr, 0, 2)
+	if w.localIP.IsValid() {
+		localIPs = append(localIPs, w.localIP)
+	}
+	if w.localIPv6.IsValid() {
+		localIPs = append(localIPs, w.localIPv6)
+	}
+
+	tunDevice, netStack, err := netstack.CreateNetTUN(localIPs, w.dnsServers, w.mtu)
+	if err != nil {
+		return err
+	}
+
+	logger := &device.Logger{
+		Verbosef: func(format string, args ...any) {
+			log.Debug().Msgf("[WireGuard] [%s] "+strings.ToLower(format), append([]any{w.threadId}, args...)...)
+		},
+		Errorf: func(format string, args ...any) {
+			log.Error().Msgf("[WireGuard] [%s] "+strings.ToLower(format), append([]any{w.threadId}, args...)...)
+		},
+	}
+
+	wgDevice := device.NewDevice(tunDevice, wgBind, logger)
+
+	log.Debug().Strs("config", w.uapiConf).Msgf("[WireGuard] initial wireguard proxy %s", w.threadId)
+
+	err = wgDevice.IpcSet(strings.Join(w.uapiConf, "\n"))
+	if err != nil {
+		return err
+	}
+
+	w.bind = wgBind
+	w.tunDevice = tunDevice
+	w.netStack = netStack
+	w.wgDevice = wgDevice
+	w.uapiConf = nil
+	w.dnsServers = nil
+	return nil
 }
 
 func NewWireGuard(option WireGuardOption) (*WireGuard, error) {
@@ -152,37 +207,31 @@ func NewWireGuard(option WireGuardOption) (*WireGuard, error) {
 		uapiConf = append(uapiConf, fmt.Sprintf("preshared_key=%s", hex.EncodeToString(bytes)))
 	}
 
-	endpointIP, err := resolver.ResolveProxyServerHost(option.Server)
-	if err != nil {
-		return nil, fmt.Errorf("parse wireguard server endpoint [%s] failure, cause: %w", option.Server, err)
-	}
-	endpoint := netip.AddrPortFrom(endpointIP, uint16(option.Port))
-	uapiConf = append(uapiConf, fmt.Sprintf("endpoint=%s", endpoint))
-
-	localIPs := make([]netip.Addr, 0, 2)
+	var (
+		localIP   netip.Addr
+		localIPv6 netip.Addr
+	)
 	if option.IP != "" {
-		if ip, err1 := netip.ParseAddr(option.IP); err1 != nil {
-			return nil, fmt.Errorf("parse wireguard ip address failure, cause: %w", err1)
-		} else {
-			localIPs = append(localIPs, ip)
+		option.IP, _, _ = strings.Cut(option.IP, "/")
+		if localIP, err = netip.ParseAddr(option.IP); err != nil {
+			return nil, fmt.Errorf("parse wireguard ip address failure, cause: %w", err)
 		}
 	}
 
 	if option.IPv6 != "" {
-		if ip, err1 := netip.ParseAddr(option.IPv6); err1 != nil {
-			return nil, fmt.Errorf("parse wireguard ipv6 address failure, cause: %w", err1)
-		} else {
-			localIPs = append(localIPs, ip)
+		option.IPv6, _, _ = strings.Cut(option.IPv6, "/")
+		if localIPv6, err = netip.ParseAddr(option.IPv6); err != nil {
+			return nil, fmt.Errorf("parse wireguard ipv6 address failure, cause: %w", err)
 		}
 	}
 
-	if len(localIPs) == 0 {
+	if !localIP.IsValid() && !localIPv6.IsValid() {
 		return nil, errors.New("wireguard missing local ip")
 	}
 
 	dns := option.DNS
 	if len(dns) == 0 {
-		dns = append(dns, "1.1.1.1")
+		dns = append(dns, "1.1.1.1", "8.8.8.8")
 	}
 	dnsServers := make([]netip.Addr, len(dns))
 	for _, d := range dns {
@@ -193,22 +242,10 @@ func NewWireGuard(option WireGuardOption) (*WireGuard, error) {
 		}
 	}
 
-	var (
-		hasIP4 bool
-		hasIP6 bool
-	)
-	for _, ip := range localIPs {
-		if ip.Is4() {
-			hasIP4 = true
-		} else {
-			hasIP6 = true
-		}
-	}
-
-	if hasIP4 {
+	if localIP.IsValid() {
 		uapiConf = append(uapiConf, "allowed_ip=0.0.0.0/0")
 	}
-	if hasIP6 {
+	if localIPv6.IsValid() {
 		uapiConf = append(uapiConf, "allowed_ip=::/0")
 	}
 
@@ -229,8 +266,8 @@ func NewWireGuard(option WireGuardOption) (*WireGuard, error) {
 			rmark: option.RoutingMark,
 		},
 		dialer:     &wgDialer{},
-		endpoint:   endpoint,
-		localIPs:   localIPs,
+		localIP:    localIP,
+		localIPv6:  localIPv6,
 		dnsServers: dnsServers,
 		uapiConf:   uapiConf,
 		threadId:   threadId,
@@ -239,39 +276,22 @@ func NewWireGuard(option WireGuardOption) (*WireGuard, error) {
 	return wireGuard, nil
 }
 
-func initWireGuard(wg *WireGuard) error {
-	wgBind := wireguard.NewWgBind(context.Background(), wg.dialer, wg.endpoint)
+type wgDialer struct {
+	options []dialer.Option
+}
 
-	tunDevice, netStack, err := netstack.CreateNetTUN(wg.localIPs, wg.dnsServers, wg.mtu)
-	if err != nil {
-		return fmt.Errorf("initial wireguard proxy %s failure, cause: %w", wg.threadId, err)
-	}
+func (d *wgDialer) DialContext(ctx context.Context, network string, address netip.AddrPort) (net.Conn, error) {
+	return dialer.DialContext(ctx, network, address.String(), d.options...)
+}
 
-	logger := &device.Logger{
-		Verbosef: func(format string, args ...any) {
-			log.Debug().Msgf("[WireGuard] [%s] "+strings.ToLower(format), append([]any{wg.threadId}, args...)...)
-		},
-		Errorf: func(format string, args ...any) {
-			log.Error().Msgf("[WireGuard] [%s] "+strings.ToLower(format), append([]any{wg.threadId}, args...)...)
-		},
-	}
+func (d *wgDialer) ListenPacket(ctx context.Context, _ netip.AddrPort) (net.PacketConn, error) {
+	return dialer.ListenPacket(ctx, "udp", "", d.options...)
+}
 
-	wgDevice := device.NewDevice(tunDevice, wgBind, logger)
+type wgConn struct {
+	net.Conn
+}
 
-	log.Debug().Strs("config", wg.uapiConf).Msg("[WireGuard] initial wireguard")
-
-	err = wgDevice.IpcSet(strings.Join(wg.uapiConf, "\n"))
-	if err != nil {
-		return fmt.Errorf("initial wireguard proxy %s failure, cause: %w", wg.threadId, err)
-	}
-
-	wg.bind = wgBind
-	wg.tunDevice = tunDevice
-	wg.netStack = netStack
-	wg.wgDevice = wgDevice
-
-	runtime.SetFinalizer(wg, func(w *WireGuard) {
-		w.down()
-	})
-	return nil
+type wgPConn struct {
+	net.PacketConn
 }
