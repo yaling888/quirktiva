26567392f931bc8bcc26591fc9b54dc8310a972a
yaling888 <73897884+yaling888@users.noreply.github.com>
2023-04-28 05:00:00 +0800

    Feature: add provider proxies API

--- hub/route/provider.go
+++ hub/route/provider.go
@@ -7,7 +7,9 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 	"github.com/phuslu/log"
+	"github.com/samber/lo"
 
+	C "github.com/Dreamacro/clash/constant"
 	"github.com/Dreamacro/clash/constant/provider"
 	"github.com/Dreamacro/clash/tunnel"
 )
@@ -16,11 +18,22 @@ func proxyProviderRouter() http.Handler {
 	r := chi.NewRouter()
 	r.Get("/", getProviders)
 
-	r.Route("/{name}", func(r chi.Router) {
+	r.Route("/{providerName}", func(r chi.Router) {
 		r.Use(parseProviderName, findProviderByName)
 		r.Get("/", getProvider)
 		r.Put("/", updateProvider)
 		r.Get("/healthcheck", healthCheckProvider)
+		r.Mount("/", proxyProviderProxyRouter())
+	})
+	return r
+}
+
+func proxyProviderProxyRouter() http.Handler {
+	r := chi.NewRouter()
+	r.Route("/{name}", func(r chi.Router) {
+		r.Use(parseProxyName, findProviderProxyByName)
+		r.Get("/", getProxy)
+		r.Get("/healthcheck", getProxyDelay)
 	})
 	return r
 }
@@ -58,7 +71,7 @@ func healthCheckProvider(w http.ResponseWriter, r *http.Request) {
 
 func parseProviderName(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		name := getEscapeParam(r, "name")
+		name := getEscapeParam(r, "providerName")
 		ctx := context.WithValue(r.Context(), CtxKeyProviderName, name)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
@@ -76,6 +89,27 @@ func findProviderByName(next http.Handler) http.Handler {
 		}
 
 		ctx := context.WithValue(r.Context(), CtxKeyProvider, provider)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func findProviderProxyByName(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			name = r.Context().Value(CtxKeyProxyName).(string)
+			pd   = r.Context().Value(CtxKeyProvider).(provider.ProxyProvider)
+		)
+		proxy, exist := lo.Find(pd.Proxies(), func(proxy C.Proxy) bool {
+			return proxy.Name() == name
+		})
+
+		if !exist {
+			render.Status(r, http.StatusNotFound)
+			render.JSON(w, r, ErrNotFound)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), CtxKeyProxy, proxy)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
