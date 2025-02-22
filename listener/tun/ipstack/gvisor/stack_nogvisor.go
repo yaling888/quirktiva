57146951336ffe60f8905a1c50a0b2961f23474f
yaling888 <73897884+yaling888@users.noreply.github.com>
2025-02-23 04:00:00 +0800

    Fix: build tag nogvisor

--- listener/tun/ipstack/gvisor/stack_nogvisor.go
+++ listener/tun/ipstack/gvisor/stack_nogvisor.go
@@ -12,6 +12,6 @@ import (
 	"github.com/yaling888/quirktiva/listener/tun/ipstack"
 )
 
-func New(device.Device, []C.DNSUrl, netip.Prefix, chan<- C.ConnContext, chan<- *inbound.PacketAdapter) (ipstack.Stack, error) {
+func New(device.Device, []C.DNSUrl, netip.Prefix, netip.Prefix, chan<- C.ConnContext, chan<- *inbound.PacketAdapter) (ipstack.Stack, error) {
 	return nil, fmt.Errorf("gVisor is not supported on this platform")
 }
